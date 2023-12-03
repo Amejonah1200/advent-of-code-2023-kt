d0770b8f58dbccdf4dc6725b8235265eebdf5903
Amejonah1200 <amejonah@gmail.com>
2023-12-03 01:34:04 +0100

    Update Utils.kt

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,15 @@
+package utils
+
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
+import kotlin.io.path.exists
 import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("inputs/$name.txt").takeIf { it.exists() }?.readLines()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +22,5 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun checkEqual(result: Int, expected: Int) = check(result == expected) { "Expected $expected, got $result" }
