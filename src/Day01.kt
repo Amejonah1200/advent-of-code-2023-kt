b14a2ff3d5ea3ae04ce5bddcb89530de30944f85
Amejonah1200 <amejonah@gmail.com>
2023-12-03 01:34:22 +0100

    Add day1 and day2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
+package day1
+
+import utils.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val r = Regex("\\d")
+        return input.asSequence().map { l -> r.findAll(l).map { it.value }.toList() }
+            .map { (it.first() + it.last()).toInt() }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = buildMap {
+            put("one", "1")
+            put("two", "2")
+            put("three", "3")
+            put("four", "4")
+            put("five", "5")
+            put("six", "6")
+            put("seven", "7")
+            put("eight", "8")
+            put("nine", "9")
+        }
+        val r = Regex("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\\d)")
+        return input.sumOf { l ->
+            buildList {
+                var m = r.find(l)
+                while (m != null) {
+                    add(numbers[m.value] ?: m.value)
+                    m = r.find(l, m.range.first + 1)
+                }
+            }.let { (it.first() + it.last()).toInt() }
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    check(part1(readInput("Day01_test1") ?: return) == 142)
+    check(part2(readInput("Day01_test2") ?: return) == 281)
+
+    val input = readInput("Day01") ?: return
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day02.kt
@@ -0,0 +1,37 @@
+package day2
+
+import utils.*
+
+val rs = Regex("(?:\\d+ .+?,?)+(;|\\Z)")
+val rc = Regex("(\\d+) ([rgb])")
+
+data class GameSet(val r: Int, val g: Int, val b: Int)
+
+fun parseGame(l: String) = rs.findAll(l).map {
+    rc.findAll(it.value).associate { it.groupValues[2] to it.groupValues[1].toInt() }
+}.map { set -> GameSet(set["r"] ?: 0, set["g"] ?: 0, set["b"] ?: 0) }.toList()
+
+fun main() {
+
+    fun part1(input: List<List<GameSet>>, r: Int, g: Int, b: Int): Int =
+        input.asSequence()
+            .mapIndexedNotNull { index, s -> if (s.all { set -> set.r <= r && set.g <= g && set.b <= b }) index + 1 else null }
+            .sum()
+
+    fun part2(input: List<List<GameSet>>): Int {
+
+        fun List<GameSet>.minGame() = GameSet(this.maxOf { it.r }, this.maxOf { it.g }, this.maxOf { it.b })
+        fun GameSet.power() = maxOf(r, 1) * maxOf(g, 1) * maxOf(b, 1)
+        return input.sumOf { it.minGame().power() }
+    }
+
+
+    checkEqual(part1((readGames("Day02_test1") ?: return), 12, 13, 14), 8)
+    checkEqual(part2(readGames("Day02_test2") ?: return), 2286)
+
+    val input = readGames("Day02") ?: return
+    part1(input, 12, 13, 14).println()
+    part2(input).println()
+}
+
+private fun readGames(s: String) = readInput(s)?.map(::parseGame)
